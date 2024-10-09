630cad341a3477fd1a6a6701d2b4fb87c3be99b7
mosesintech <mosesmaxcosme@gmail.com>
2024-10-08 20:46:38 -0500

    feat(scenes): add unique dialogue to IronclawPort-1

--- src/Scenes/IronclawPort-1/Dialogues.ts
+++ src/Scenes/IronclawPort-1/Dialogues.ts
@@ -1,15 +1,66 @@
 export const IronclawPortDialogues = [
   {
     actor: 'Delsaran', // should match the name of the actor entity
-    text: 'Press SHIFT to run!',
+    text: `I'm Delsaran! You can hold left SHIFT down to run.`,
+  },
+  {
+    actor: 'Wolfkin Guard One',
+    text: `I'm Wolfkin Guard One!`,
+  },
+  {
+    actor: 'Wolfkin Guard Two',
+    text: `I'm Wolfkin Guard Two!`,
+  },
+  {
+    actor: 'Wolfkin Guard Three',
+    text: `I'm Wolfkin Guard Three!`,
+  },
+  {
+    actor: 'Wolfkin Guard Four',
+    text: `I'm Wolfkin Guard Four!`,
+  },
+  {
+    actor: 'Wolfkin Guard Five',
+    text: `I'm Wolfkin Guard Five!`,
+  },
+  {
+    actor: 'Wolfkin Guard Six',
+    text: `I'm Wolfkin Guard Six!`,
+  },
+  {
+    actor: 'Wolfkin Guard Seven',
+    text: `I'm Wolfkin Guard Seven!`,
+  },
+  {
+    actor: 'Wolfkin Guard Eight',
+    text: `I'm Wolfkin Guard Eight!`,
+  },
+  {
+    actor: 'Wolfkin Guard Nine',
+    text: `I'm Wolfkin Guard Nine!`,
+  },
+  {
+    actor: 'Wolfkin Citizen One',
+    text: `I'm Wolfkin Citizen One!`,
+  },
+  {
+    actor: 'Wolfkin Cleric One',
+    text: `I'm Wolfkin Cleric One!`,
+  },
+  {
+    actor: 'Wolfkin Citizen Two',
+    text: `I'm Wolfkin Citizen Two!`,
+  },
+  {
+    actor: 'Wolfkin Citizen Three',
+    text: `I'm Wolfkin Citizen Three!`,
+  },
+  {
+    actor: 'Wolfkin Citizen Four',
+    text: `I'm Wolfkin Citizen Four!`,
   },
   {
     actor: 'default', // when no actor or object can be found
     text: 'Nothing interesting over here.',
   },
 ];
-
-type Dialogues = {
-  actor: string;
-  text: string;
-}[];
